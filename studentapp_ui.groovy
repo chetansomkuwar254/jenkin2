79d621de97d143ce65fa16edec58fb4b179bc9d9
jarvis <chetansomkuwar@gmail.com>
2024-04-06 17:54:44 +0530

    --- Auto Git Commit ---

--- studentapp_ui.groovy
+++ studentapp_ui.groovy
@@ -10,7 +10,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                echo 'Here we are pullin our project repository'
+                echo 'Here we doing build operation'
             }
         }        
     }
