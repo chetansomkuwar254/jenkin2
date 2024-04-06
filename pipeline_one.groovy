751b4b105d25a1b53dc990997445e6e97a423c6d
jarvis <chetansomkuwar@gmail.com>
2024-04-06 17:35:08 +0530

    --- Auto Git Commit ---

--- pipeline_one.groovy
+++ pipeline_one.groovy
@@ -9,25 +9,25 @@ pipeline {
                 echo 'Pull Done'
             }
         }
-    //     stage('Build') {
-    //         steps {
-    //             echo 'Buld Done'
-    //         }
-    //     }
-    //     stage('Test') {
-    //         steps {
-    //             echo 'Test Done'
-    //         }
-    //     }
-    //     stage('Qulaity-Test') {
-    //         steps {
-    //             echo 'Quality-Check Done'
-    //         }
-    //     }
-    //     stage('Deploy') {
-    //         steps {
-    //             echo 'Deploy Done'
-    //         }
-    //     }
-    }
+    // //     stage('Build') {
+    // //         steps {
+    // //             echo 'Buld Done'
+    // //         }
+    // //     }
+    // //     stage('Test') {
+    // //         steps {
+    // //             echo 'Test Done'
+    // //         }
+    // //     }
+    // //     stage('Qulaity-Test') {
+    // //         steps {
+    // //             echo 'Quality-Check Done'
+    // //         }
+    // //     }
+    // //     stage('Deploy') {
+    // //         steps {
+    // //             echo 'Deploy Done'
+    // //         }
+    // //     }
+    // }
 }
