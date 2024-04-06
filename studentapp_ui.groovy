d68350a386bd43c16b0da80b9de3afe4084f1be3
jarvis <chetansomkuwar@gmail.com>
2024-04-06 17:53:23 +0530

    --- Auto Git Commit ---

--- studentapp_ui.groovy
+++ studentapp_ui.groovy
@@ -8,5 +8,11 @@ pipeline {
                 echo 'Here we are pullin our project repository'
             }
         }
+        stage('Pull') {
+            steps {
+                git branch: 'main', changelog: false, credentialsId: 'ssh-key', poll: false, url: 'https://github.com/chetansomkuwar254/jenkins-b2'
+                echo 'Here we are pullin our project repository'
+            }
+        }
     }
 }
