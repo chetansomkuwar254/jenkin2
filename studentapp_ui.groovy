043323225b1d85f54a926555ed52587336c5d6ff
chetansomkuwar254 <146824400+chetansomkuwar254@users.noreply.github.com>
2024-04-07 19:45:24 +0000

    --- Auto Git Commit ---

--- studentapp_ui.groovy
+++ studentapp_ui.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Pull') {
             steps {
-                git branch: 'main', credentialsId: 'jarvis-key', url: 'https://github.com/chetansomkuwar254/studentapp-ui-jenkins-b2.git'
+                git 'https://github.com/chetansomkuwar254/studentapp.ui.git'
                 echo 'Here we are pullin our project repository'
             }
         }
