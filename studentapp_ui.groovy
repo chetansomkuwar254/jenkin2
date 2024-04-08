58a163150793800c0846047171270b533d6abbd3
chetansomkuwar254 <146824400+chetansomkuwar254@users.noreply.github.com>
2024-04-08 12:07:07 +0000

    --- Auto Git Commit ---

--- studentapp_ui.groovy
+++ studentapp_ui.groovy
@@ -11,7 +11,7 @@ pipeline {
         stage('Build') {
             steps {
                 sh 'mvn clean package'
-                
+                echo 'Yes, Application Build pull is done !'
             }
         }
         stage('Test') {
